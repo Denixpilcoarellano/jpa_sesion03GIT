df50ddb2eae328881bd4d6357b9946e9be4ed49e
Junior <rodriguezjunior219@gmail.com>
2023-09-15 22:16:45 -0500

    cambios

--- src/main/java/main/pruebas.java
+++ src/main/java/main/pruebas.java
@@ -2,9 +2,11 @@ package main;
 
 import java.util.List;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -15,8 +17,19 @@ public class pruebas {
 		System.out.println("--listar usuario--");
 		EntityManagerFactory fabrica=Persistence.createEntityManagerFactory("JPA_Sesion01");
 		EntityManager em=fabrica.createEntityManager();
-		List<TbUsuario> listUsuario=em.createQuery("select a from TbUsuario a",TbUsuario.class).getResultList();
-        System.out.print("nro de usuario : "+listUsuario.size());
+		
+		Query query=em.createNativeQuery("{ call usp_validaAcceso(?,?)}",TbUsuario.class);
+		query.setParameter(1,"U001@gmail.com");
+		query.setParameter(2,"10001");
+		TbUsuario u=(TbUsuario)query.getSingleResult();
+		if(u == null){
+			System.out.print("usuario no existe");
+			
+		}else{
+			System.out.print("bienvenido : " + u.getNomUsua());
+			
+		}
+		System.out.print("----------------------------------------------------");
 	}
 
 }
